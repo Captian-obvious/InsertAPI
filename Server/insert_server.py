642692fd13c0fff42c9c9b2c23ccf36d9089d62d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 12:23:06 -0400

    Create insert_server.py

---
+++ Server/insert_server.py
@@ -0,0 +1,22 @@
+class insertserver:
+    class downloader:
+        def downloadAsset(assetid):
+            url = 'https://assetdelivery.roblox.com/v1/asset/?id='+str(assetid)
+            request = requests.get(url)
+            if (request.status_code==200):
+                rawData = request.content
+                f = open('assets/v1/'+str(assetid), "wb")
+                f.write(str(rawData))
+                return {'Asset': f, 'Success': True}
+            else:
+                return {'Success':False,'StatusCode': requests.status_code, 'ErrorMessage': requests.status}
+            ##endif
+        ##end
+    ##end
+    class compiler:
+        def compileAsset(asset):
+            data = asset
+            return str(jsonify(data))
+        ##end
+    ##end
+##end

153f7b436dbd2cee8290a9d996f7c8527f10388b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 19:53:16 -0400

    Update index.py

--- Server/index.py
+++ Server/index.py
@@ -10,6 +10,15 @@ def index():
     return """
     <!DOCTYPE html>
     <html>
+        <head>
+            <title>Insert Cloud API - LNDP</title>
+            <link rel='icon' href='/images/favicon.ico'/>
+            <link rel='stylesheet' href='/css/styles-main.css'/>
+        </head>
+        <body>
+            <h1 class='red1 center ta_c'>Insert API Server:</h1>
+            <p class='red1 center ta_c'>IDK what to put here. (Landing Page).</p>
+        </body>
     </html>
     """
 ##end

4eec6d0cb902fca4edb906c4f1f16acfdd56751b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 19:22:53 -0400

    Update UnionOperation.py

--- Server/UnionOperation.py
+++ Server/UnionOperation.py
@@ -1,19 +1,15 @@
 import rbxm
 
-# Load the rbxm file
-file = rbxm.load('model.rbxm')
-
-# Iterate over the objects in the file
-for object in file.objects:
-    #Check if the object is a UnionOperation
-    if isinstance(object, rbxm.UnionOperation):
-        #Print the current name of the UnionOperation
-        print(object.Name)
-        #Change the name of the UnionOperation
-        object.Name = 'NewUnion'
-        #Print the new name of the UnionOperation
-        print(object.Name)
-    ##endif
+def FormatUnionParts(f):
+    # Load the rbxm file
+    file = rbxm.load(f)
+    
+    # Iterate over the objects in the file
+    for object in file.objects:
+        #Check if the object is a UnionOperation
+        if isinstance(object, rbxm.UnionOperation):
+            #Print the current name of the UnionOperation
+            parts = object.Parts
+        ##endif
+    ##end
 ##end
-
-rbxm.save(file, 'model.rbxm')

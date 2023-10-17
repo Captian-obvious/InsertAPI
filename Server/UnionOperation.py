490638bef3064bde4bbac68534fd6f8223f866a1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 19:26:00 -0400

    Update UnionOperation.py

--- Server/UnionOperation.py
+++ Server/UnionOperation.py
@@ -3,8 +3,7 @@ import rbxm
 def FormatUnionParts(f):
     # Load the rbxm file
     file = rbxm.load(f)
-    
-    # Iterate over the objects in the file
+    #Sort through the objects in the file.
     for object in file.objects:
         #Check if the object is a UnionOperation
         if isinstance(object, rbxm.UnionOperation):

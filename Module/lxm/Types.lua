e3f5426eff11d1790d421b3a2ade798342fe58c4
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 11:27:28 -0400

    Update Types.lua

--- Module/lxm/Types.lua
+++ Module/lxm/Types.lua
@@ -1,31 +1,31 @@
 export type Buffer = {
-Offset: number,
-Source: string,
-Length: number,
-IsFinished: boolean,
-LastUnreadBytes: number,
-AllowOverflows: boolean,
-
-read: (Buffer, len: number?, shiftOffset: boolean?) -> string,
-readNumber: (Buffer, packfmt: string?, shift: boolean?) -> number,
-seek: (Buffer, len: number) -> (),
-append: (Buffer, newData: string) -> (),
-toEnd: (Buffer) -> ()
+    Offset: number,
+    Source: string,
+    Length: number,
+    IsFinished: boolean,
+    LastUnreadBytes: number,
+    AllowOverflows: boolean,
+    
+    read: (Buffer, len: number?, shiftOffset: boolean?) -> string,
+    readNumber: (Buffer, packfmt: string?, shift: boolean?) -> number,
+    seek: (Buffer, len: number) -> (),
+    append: (Buffer, newData: string) -> (),
+    toEnd: (Buffer) -> ()
 }
 
 export type Chunk = {
-InternalID: number,
-Header: "END\0"|"INST"|"META"|"PRNT"|"PROP"|"SIGN"|"SSTR",
-Data: Buffer,
-Error: (Chunk, string) -> ()
+    InternalID: number,
+    Header: "END\0"|"INST"|"META"|"PRNT"|"PROP"|"SIGN"|"SSTR",
+    Data: Buffer,
+    Error: (Chunk, string) -> ()
 }
 
 export type VirtualInstance = {
-ClassId: number,
-ClassName: string,
-Properties: {[string]: any},
-Ref: number,
-Children: {VirtualInstance}
+    ClassId: number,
+    ClassName: string,
+    Properties: {[string]: any},
+    Ref: number,
+    Children: {VirtualInstance}
 }
 
 export type Rbxm = {

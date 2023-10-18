a6383378fdb7cf19444a210d3629e8d858316d52
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-17 20:30:55 -0400

    Update compiler.py

--- Server/compiler.py
+++ Server/compiler.py
@@ -1,5 +1,17 @@
 import base64,os,sys,rbxm,requests,robloxapi
 
-def compileAsset(data):
+def parseObject():
+    if isinstance(object, rbxm.UnionOperation):
+        parts = object.Parts
+    ##endif
+##end
+
+def parseAsset(data):
+    class data:
+        isFinished = False
+    ##end
     file = rbxm.load(data)
+    for object in file.objects:
+        parseObject(object,data)
+    ##end
 ##end
